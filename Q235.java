14c91ede337153a12e50b1701c20d360e75f490c
zuohuajie <huajie.zuo@ly.com>
2022-03-11 11:54:31 +0800

    feat

---
+++ Q235.java
@@ -0,0 +1,20 @@
+/**
+ * Description:
+ * date: 2022/3/11 11:54
+ *
+ * @author huajie.zuo
+ */
+public class Q235 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(p.val>q.val){
+            return lowestCommonAncestor(root, q, p);
+        }
+        if(q.val<root.val){
+            return lowestCommonAncestor(root.left,p,q);
+        }else if(p.val>root.val){
+            return lowestCommonAncestor(root.right,p,q);
+        }else {
+            return root;
+        }
+    }
+}
