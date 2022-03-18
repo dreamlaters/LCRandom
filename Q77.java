bc7c380a33378d5ecd9b7ab65663cbf7d8a62232
zuohuajie <huajie.zuo@ly.com>
2022-03-18 11:15:30 +0800

    feat

---
+++ Q77.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Description:
+ * date: 2022/3/18 10:02
+ *
+ * @author huajie.zuo
+ */
+public class Q77 {
+    List<List<Integer>> res=new LinkedList<>();
+    public List<List<Integer>> combine(int n, int k) {
+        dfs(1,n,k,new LinkedList<>());
+        return res;
+    }
+    public void dfs(int index, int n, int k, LinkedList<Integer> list) {
+        if(list.size()==k){
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = index; i <= n; i++) {
+            list.add(i);
+            dfs(i+1,n,k,list);
+            list.removeLast();
+        }
+    }
+}
